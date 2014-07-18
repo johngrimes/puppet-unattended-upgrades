7d26655f63cc6a2fd184cd7e4dc0ea00a756e44e
John Grimes <john@smallspark.com.au>
2014-07-17 22:29:25 -0700

    Fix file reference

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,6 @@ class unattended_upgrades {
 
   file { '/etc/apt/apt.conf.d/20auto-upgrades':
     ensure  => present,
-    source => 'puppet:///modules/unattended_upgrades/auto_upgrades.conf'
+    source => 'puppet:///modules/unattended_upgrades/20auto-upgrades'
   }
 }
